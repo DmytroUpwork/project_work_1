4e1cecced6bd5f94fe00473bb9e33e56fa0cdbc3
Dmitrii <lomonovskij@gmail.com>
2020-07-09 18:45:51 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 
 //slide page ======================================================
 new fullpage("main", {});
+
+//scroll ==========================================================
+$("a[href*=#]").on("click", function (e) {
+  var anchor = $(this);
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(anchor.attr("href")).offset().top,
+      },
+      777
+    );
+  e.preventDefault();
+  return false;
+});
