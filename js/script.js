f7a8e3e6f55512a764388e67d41078cddfb82a57
Dmitrii <lomonovskij@gmail.com>
2020-07-09 19:38:30 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
+$(document).ready(function() {
+  //slide page ======================================================
+  new fullpage("main", {});
 
-//slide page ======================================================
-new fullpage("main", {});
+  //scroll ==========================================================
+  $("a[href*=#]").on("click", function (e) {
+    var anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor.attr("href")).offset().top,
+        },
+        777
+      );
+    e.preventDefault();
+    return false;
+  });
 
-//scroll ==========================================================
-// $("a[href*=#]").on("click", function (e) {
-//   var anchor = $(this);
-//   $("html, body")
-//     .stop()
-//     .animate(
-//       {
-//         scrollTop: $(anchor.attr("href")).offset().top,
-//       },
-//       777
-//     );
-//   e.preventDefault();
-//   return false;
-// });
+  //owl carousel ===================================================
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+});
