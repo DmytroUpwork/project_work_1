22f021d72c0f90973c66d7cec4a1c4ef958f5ef0
Dmitrii <lomonovskij@gmail.com>
2020-07-09 19:09:00 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 new fullpage("main", {});
 
 //scroll ==========================================================
-$("a[href*=#]").on("click", function (e) {
-  var anchor = $(this);
-  $("html, body")
-    .stop()
-    .animate(
-      {
-        scrollTop: $(anchor.attr("href")).offset().top,
-      },
-      777
-    );
-  e.preventDefault();
-  return false;
-});
+// $("a[href*=#]").on("click", function (e) {
+//   var anchor = $(this);
+//   $("html, body")
+//     .stop()
+//     .animate(
+//       {
+//         scrollTop: $(anchor.attr("href")).offset().top,
+//       },
+//       777
+//     );
+//   e.preventDefault();
+//   return false;
+// });
